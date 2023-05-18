86b2cc01db0e5de174f24e6a28d3de145f71b159
AndrewYoung1998 <36615410+AndrewYoung1998@users.noreply.github.com>
2023-05-18 16:44:43 -0400

    added assignment 1 to github

---
+++ ASCIIReader/src/main/java/edu/psgv/sweng861/AsciiReader.java
@@ -0,0 +1,150 @@
+package edu.psgv.sweng861;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class AsciiReader {
+
+	public static void main(String[] args) {
+		if (args.length == 0) {
+			System.out.println("Please provide the filename as an argument.");
+			return;
+		}
+
+		// Read the input file
+		String filename = args[0];
+		String fileContent = readTextFile(filename);
+
+		// Ask the user for the maximum number of characters to read
+        int maxCharacters = getMaxCharactersFromUser(fileContent.length());
+
+        // Truncate the file content to the specified maximum characters
+        fileContent = fileContent.substring(0, maxCharacters);
+		// Print the statistics
+		printStatistics(fileContent);
+
+		// Print the content of the file in reverse order, converting all letters to lower case
+		printReversedContent(fileContent);
+	}
+
+	private static String readTextFile(String filename) {
+		StringBuilder content = new StringBuilder();
+		try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+			String line;
+			while ((line = reader.readLine()) != null) {
+				content.append(line).append("\n");
+			}
+		} catch (IOException e) {
+			System.out.println("Error reading the file: " + e.getMessage());
+		}
+		return content.toString();
+	}
+
+	   private static int getMaxCharactersFromUser(int fileLength) {
+	        int maxCharacters = 0;
+	        boolean invalidInput = true;
+
+	        while (invalidInput) {
+	            try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+	                System.out.print("Enter the maximum number of characters to read (up to " + fileLength + "): ");
+	                String input = reader.readLine();
+	                maxCharacters = Integer.parseInt(input);
+
+	                if (maxCharacters > fileLength) {
+	                    System.out.println("Invalid input. The number exceeds the file length.");
+	                } else {
+	                    invalidInput = false;
+	                }
+	            } catch (IOException | NumberFormatException e) {
+	                System.out.println("Invalid input. Please enter a valid integer.");
+	            }
+	        }
+
+	        return maxCharacters;
+	    }
+
+	private static void printStatistics(String fileContent) {
+		int totalWords = countWords(fileContent);
+		int totalNumbers = countNumbers(fileContent);
+		int totalCharacters = fileContent.length();
+		int totalSpaces = countSpaces(fileContent);
+		int totalPunctuation = countPunctuation(fileContent);
+		int totalUpperCase = countUpperCase(fileContent);
+		int totalLowerCase = countLowerCase(fileContent);
+
+		System.out.println("Total Number of words in the file: " + totalWords);
+		System.out.println("Total Number of numbers in the file: " + totalNumbers);
+		System.out.println("Total Number of characters including spaces: " + totalCharacters);
+		System.out.println("Total Number of spaces: " + totalSpaces);
+		System.out.println("Total Number of punctuation characters: " + totalPunctuation);
+		System.out.println("Total Number of characters in upper case: " + totalUpperCase);
+		System.out.println("Total Number of characters in lower case: " + totalLowerCase);
+	}
+
+	private static int countWords(String text) {
+		String[] words = text.split("\\W+");
+		return words.length;
+	}
+
+	private static int countNumbers(String text) {
+		String[] numbers = text.split("\\D+");
+		return numbers.length;
+	}
+
+	private static int countSpaces(String text) {
+		int spaceCount = 0;
+		for (char c : text.toCharArray()) {
+			if (Character.isWhitespace(c)) {
+				spaceCount++;
+			}
+		}
+		return spaceCount;
+	}
+
+	private static int countPunctuation(String text) {
+		int punctuationCount = 0;
+		for (char c : text.toCharArray()) {
+			if (Character.isLetterOrDigit(c)) {
+				continue;
+			}
+			if (Character.isWhitespace(c)) {
+				continue;
+			}
+			punctuationCount++;
+		}
+		return punctuationCount;
+	}
+
+	private static int countUpperCase(String text) {
+		int upperCaseCount = 0;
+		for (char c : text.toCharArray()) {
+			if (Character.isUpperCase(c)) {
+				upperCaseCount++;
+			}
+		}
+		return upperCaseCount;
+	}
+
+	private static int countLowerCase(String text) {
+		int lowerCaseCount = 0;
+		for (char c : text.toCharArray()) {
+			if (Character.isLowerCase(c)) {
+				lowerCaseCount++;
+			}
+		}
+		return lowerCaseCount;
+	}
+
+	private static void printReversedContent(String text) {
+		StringBuilder reversedContent = new StringBuilder();
+		for (int i = text.length() - 1; i >= 0; i--) {
+			char c = text.charAt(i);
+			if (Character.isLetter(c)) {
+				reversedContent.append(Character.toLowerCase(c));
+			}
+		}
+		System.out.println("Reversed content of the file: " + reversedContent.toString());
+	}
+}
